ed82b6c9027e1da6475b3505317bfa382a104c54
asdia <71514365+asdia0@users.noreply.github.com>
2021-02-15 16:30:48 +0800

    Split solution into multiple projects

---
+++ src/Battleship.Core/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace Battleship
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text.RegularExpressions;
+    using System.Timers;
+
+    /// <summary>
+    /// Main class of the project.
+    /// </summary>
+    public class Program
+    {
+        private static Timer delayTimer;
+
+        private static int sumMoves = 0;
+
+        private static int player1 = 0;
+
+        private static int player2 = 0;
+
+        /// <summary>
+        /// Simulates a number of games.
+        /// </summary>
+        /// <param name="simulations">Number of games to simulate.</param>
+        public static void Run(int simulations)
+        {
+            decimal totalTime = 0;
+
+            string fileContent = string.Empty;
+
+            for (int i = 0; i < simulations; i++)
+            {
+                string winner = string.Empty;
+
+                Stopwatch elapsedTime = new Stopwatch();
+                elapsedTime.Start();
+
+                Game game = new Game();
+                game.CreateGame();
+
+                elapsedTime.Stop();
+
+                sumMoves += game.Move;
+                if (game.Winner == true)
+                {
+                    winner = "Player 1";
+                    player1++;
+                }
+                else
+                {
+                    winner = "Player 2";
+                    player2++;
+                }
+
+                fileContent += $"{Settings.GridHeight},{Settings.GridWidth},Random,{winner},{game.Move}\n";
+
+                totalTime += decimal.Divide(elapsedTime.ElapsedMilliseconds, 1000);
+
+                TimeSpan avg = TimeSpan.FromSeconds((double)decimal.Divide(totalTime, i + 1));
+                TimeSpan tot = TimeSpan.FromSeconds((double)totalTime);
+
+                Console.Clear();
+                Console.WriteLine($"Percent complete: {decimal.Divide(i + 1, simulations) * 100}%\nCurrent Dimension: {Settings.GridHeight}x{Settings.GridWidth}\nWhite won: {player1}\nBlack won: {player2}\nAverage moves: {decimal.Divide(sumMoves, i + 1)}\nTotal time elapsed: {tot.Hours} hours {tot.Minutes} minutes {tot.Seconds} seconds {tot.Milliseconds} milliseconds\nAverage time elapsed: {avg.Hours} hours {avg.Minutes} minutes {avg.Seconds} seconds {avg.Milliseconds} milliseconds");
+            }
+
+            //using (StreamWriter outputFile = File.AppendText("HTTest1.csv"))
+            //{
+            //    outputFile.WriteLine(fileContent);
+            //}
+        }
+
+        /// <summary>
+        /// Delays a task by a certain amount of seconds.
+        /// </summary>
+        /// <param name="time">The number of seconds to delay.</param>
+        public static void Delay(double time)
+        {
+            int i = 0;
+
+            delayTimer = new Timer();
+            delayTimer.Interval = time;
+            delayTimer.AutoReset = false;
+            delayTimer.Elapsed += (s, args) => i = 1;
+            delayTimer.Start();
+
+            while (i == 0)
+            { };
+        }
+
+        /// <summary>
+        /// Main method of the project.
+        /// </summary>
+        public static void Main()
+        {
+            Run(10);
+        }
+    }
+}
---
+++ src/Battleship.Core/Ship.cs
@@ -0,0 +1,138 @@
+﻿namespace Battleship
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Defines a ship on the grid.
+    /// </summary>
+    public class Ship
+    {
+        /// <summary>
+        /// The squares the ship had occupied.
+        /// </summary>
+        public List<Square> OriginalOccupiedSquares = new List<Square>();
+
+        /// <summary>
+        /// The ship's type.
+        /// </summary>
+        public string Type;
+
+        /// <summary>
+        /// The ship's length.
+        /// </summary>
+        public int Length;
+
+        /// <summary>
+        /// The squares that the ship currently occupies.
+        /// </summary>
+        public List<Square> CurrentOccupiedSquares = new List<Square>();
+
+        /// <summary>
+        /// The ship's grid.
+        /// </summary>
+        public Grid Grid;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ship"/> class.
+        /// </summary>
+        /// <param name="grid">The ship's grid.</param>
+        /// <param name="type">The ship's type.</param>
+        public Ship(Grid grid, string type)
+        {
+            this.Grid = grid;
+            this.Type = type;
+
+            switch (type)
+            {
+                case "Carrier":
+                    this.Length = 5;
+                    break;
+                case "Battleship":
+                    this.Length = 4;
+                    break;
+                case "Cruiser":
+                    this.Length = 3;
+                    break;
+                case "Submarine":
+                    this.Length = 3;
+                    break;
+                case "Destroyer":
+                    this.Length = 2;
+                    break;
+                default:
+                    throw new Exception("Ship type unknown.");
+            }
+        }
+
+        /// <summary>
+        /// Increases the probability of the squares that a ship can fit on.
+        /// </summary>
+        /// <param name="probability">The current probability dictionary.</param>
+        /// <param name="sq">Base square to place the ship on.</param>
+        /// <param name="alignment">Alignment of the ship.</param>
+        /// <returns>The new probability dictionary.</returns>
+        public Dictionary<int, int> IncreaseProbability(Dictionary<int, int> probability,  Square sq, bool alignment)
+        {
+            Dictionary<int, int> res = probability;
+
+            int availRows = Settings.GridWidth - (sq.ID % Settings.GridHeight);
+            int availCols = Settings.GridHeight - (int)Math.Floor((double)(sq.ID / Settings.GridWidth));
+
+            // horizontal
+            if (alignment && this.Length <= availRows)
+            {
+                int failures = 0;
+
+                for (int j = 0; j < this.Length; j++)
+                {
+                    Square square = this.Grid.Squares[j + sq.ID];
+                    if ((square.BeenSearched && square.HadShip == true) || square.IsSunk == true)
+                    {
+                        failures++;
+                    }
+                }
+
+                if (failures == 0)
+                {
+                    for (int j = 0; j < this.Length; j++)
+                    {
+                        if (res.ContainsKey(j + sq.ID))
+                        {
+                            res[j + sq.ID]++;
+                        }
+                    }
+                }
+            }
+
+            // vertical
+            if (!alignment && this.Length <= availCols)
+            {
+                int failures = 0;
+
+                for (int j = 0; j < this.Length; j++)
+                {
+                    Square square = this.Grid.Squares[(j * Settings.GridWidth) + sq.ID];
+                    if ((square.BeenSearched && square.HadShip == true) || square.IsSunk == true)
+                    {
+                        failures++;
+                    }
+                }
+
+                if (failures == 0)
+                {
+                    for (int j = 0; j < this.Length; j++)
+                    {
+                        if (res.ContainsKey((j * Settings.GridWidth) + sq.ID))
+                        {
+                            res[(j * Settings.GridWidth) + sq.ID]++;
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
