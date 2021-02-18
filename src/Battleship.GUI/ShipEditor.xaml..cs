07b9ac4e9e822323e6c66ce2de97b42434b504dc
asdia <71514365+asdia0@users.noreply.github.com>
2021-02-19 05:43:34 +0800

    designed ShipEditor

--- src/Battleship.GUI/ShipEditor.xaml..cs
+++ src/Battleship.GUI/ShipEditor.xaml..cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Battleship.GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ShipEditor : Window
     {
-        public MainWindow()
+        public ShipEditor()
         {
             InitializeComponent();
-            Application.Current.MainWindow.WindowState = WindowState.Maximized;
-        }
-
-        public void Click_ShipEditor(object sender, RoutedEventArgs e)
-        {
-            Window shipeditor = new ShipEditor();
-            shipeditor.Show();
         }
     }
 }
