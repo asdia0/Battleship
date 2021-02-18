47c66cd6e44c1b4f4731c864e0e3e10b5d55b270
asdia <71514365+asdia0@users.noreply.github.com>
2021-02-19 07:23:03 +0800

    added click events

--- src/Battleship.GUI/MainWindow.xaml.cs
+++ src/Battleship.GUI/MainWindow.xaml.cs
@@ -26,10 +26,68 @@ namespace Battleship.GUI
             Application.Current.MainWindow.WindowState = WindowState.Maximized;
         }
 
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            Application.Current.Shutdown();
+        }
+
+        // FILE
+        public void Click_New(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Click_Load(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Click_Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // EDIT
         public void Click_ShipEditor(object sender, RoutedEventArgs e)
         {
-            Window shipeditor = new ShipEditor();
-            shipeditor.Show();
+            Window shipEditor = new ShipEditor();
+            shipEditor.Show();
+        }
+
+        public void Click_GridEditor(object sender, RoutedEventArgs e)
+        {
+            Window gridEditor = new GridEditor();
+            gridEditor.Show();
+        }
+
+        // MODE
+        public void Click_Play(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Click_Simulate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Click_Find(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        // HELP
+        public void Click_About(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Click_Controls(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
