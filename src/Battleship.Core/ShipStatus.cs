68871cc092acf4e9d669d05afb04810e54750e35
asdia0 <asdia.the.potato@gmail.com>
2021-06-30 21:22:39 +0800

    Add ShipStatus

---
+++ src/Battleship.Core/ShipStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace Battleship.Core
+{
+    /// <summary>
+    /// Types of <see cref="Ship"/> statuses.
+    /// </summary>
+    public enum ShipStatus
+    {
+        /// <summary>
+        /// Ship is operational (<see cref="Ship.UnsearchedSquares"/> > 0).
+        /// </summary>
+        Operational,
+
+        /// <summary>
+        /// Ship has been sunk (<see cref="Ship.UnsearchedSquares"/> = 0).
+        /// </summary>
+        Sunk,
+    }
+}
