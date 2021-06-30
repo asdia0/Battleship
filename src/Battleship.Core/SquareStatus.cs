46880cce814b422bc656ac84bce3f06ab3ce37e8
asdia0 <asdia.the.potato@gmail.com>
2021-06-30 20:16:24 +0800

    Add SquareStatus

---
+++ src/Battleship.Core/SquareStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace Battleship.Core
+{
+    /// <summary>
+    /// Types of statuses for a <see cref="Square"/>.
+    /// </summary>
+    public enum SquareStatus
+    {
+        /// <summary>
+        /// Square is unsearched (<see cref="Square.Searched"/> = false).
+        /// </summary>
+        Unsearched,
+
+        /// <summary>
+        /// Square is a miss (<see cref="Square.Searched"/> = true AND <see cref="Square.Ship"/> = null).
+        /// </summary>
+        Miss,
+
+        /// <summary>
+        /// Square was sunk (<see cref="Square.Searched"/> = true AND <see cref="Square.Ship"/> != null).
+        /// </summary>
+        Sunk,
+    }
+}
