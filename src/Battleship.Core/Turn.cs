3b7123cbb832a23ae81fc47c2363db2ac085b21f
asdia0 <asdia.the.potato@gmail.com>
2021-06-30 22:28:08 +0800

    Add Turn

---
+++ src/Battleship.Core/Turn.cs
@@ -0,0 +1,18 @@
+﻿namespace Battleship.Core
+{
+    /// <summary>
+    /// Types of player turns.
+    /// </summary>
+    public enum Turn
+    {
+        /// <summary>
+        /// Player 1's turn.
+        /// </summary>
+        Player1,
+
+        /// <summary>
+        /// Player 2's turn.
+        /// </summary>
+        Player2,
+    }
+}
