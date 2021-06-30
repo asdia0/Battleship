052e1045e1191bfa9bea54d8e67e3376fb8567e6
asdia0 <asdia.the.potato@gmail.com>
2021-06-30 20:05:34 +0800

    Add Position

---
+++ src/Battleship.Core/Position.cs
@@ -0,0 +1,38 @@
+﻿namespace Battleship.Core
+{
+    /// <summary>
+    /// Defines a coordinate.
+    /// </summary>
+    public struct Position
+    {
+        /// <summary>
+        /// Gets the X-coordinate.
+        /// </summary>
+        public int X { get; }
+
+        /// <summary>
+        /// Gets the Y-coordinate.
+        /// </summary>
+        public int Y { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Position"/> struct.
+        /// </summary>
+        /// <param name="x">The x-coodinate.</param>
+        /// <param name="y">The y-coordinate.</param>
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        /// <summary>
+        /// Converts the <see cref="Position"/> into a <see cref="string"/>.
+        /// </summary>
+        /// <returns>The <see cref="Position"/> as a <see cref="string"/>.</returns>
+        public override string ToString()
+        {
+            return $"({this.X}, {this.Y})";
+        }
+    }
+}
