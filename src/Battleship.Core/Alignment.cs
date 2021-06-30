c73b4c87e7e5f7c29b1eb2b519541d1319647077
asdia0 <asdia.the.potato@gmail.com>
2021-06-30 20:20:14 +0800

    Add Alignment

---
+++ src/Battleship.Core/Alignment.cs
@@ -0,0 +1,18 @@
+﻿namespace Battleship.Core
+{
+    /// <summary>
+    /// Types of <see cref="Ship"/> alignment.
+    /// </summary>
+    public enum Alignment
+    {
+        /// <summary>
+        /// Horizontal alignment.
+        /// </summary>
+        Horizontal,
+
+        /// <summary>
+        /// Vertical alignment.
+        /// </summary>
+        Vertical
+    }
+}
