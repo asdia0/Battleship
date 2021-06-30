68859ce3bd4ecb0f67b2506703c76245f3296b4d
asdia0 <asdia.the.potato@gmail.com>
2021-06-30 19:44:16 +0800

    Add BattleshipException

---
+++ src/Battleship.Core/BattleshipException.cs
@@ -0,0 +1,24 @@
+﻿namespace Battleship.Core
+{
+    using System;
+
+    /// <summary>
+    /// Defines an exception thrown in this project.
+    /// </summary>
+    public class BattleshipException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BattleshipException"/> class.
+        /// </summary>
+        public BattleshipException()
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BattleshipException"/> class.
+        /// </summary>
+        /// <param name="message">A message about the exception.</param>
+        public BattleshipException(string message)
+            : base(message)
+        { }
+    }
+}
