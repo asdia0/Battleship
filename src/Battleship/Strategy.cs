fdb749aef69918a5abb674c20e0088b2f2beff81
asdia0 <asdia.the.potato@gmail.com>
2021-07-09 15:35:46 +0800

    Add Strategy

---
+++ src/Battleship/Strategy.cs
@@ -0,0 +1,23 @@
+﻿namespace Battleship
+{
+    /// <summary>
+    /// Types of Battleship strategies.
+    /// </summary>
+    public enum Strategy
+    {
+        /// <summary>
+        /// Moves are made at random.
+        /// </summary>
+        Random,
+
+        /// <summary>
+        /// If there are no hit squares, moves are made at random. Else, a random square adjacent to a hit square will be searched.
+        /// </summary>
+        HuntTarget,
+
+        /// <summary>
+        /// Goes through all possible ship arrangments and finds the square with the highest proability of having a ship on it.
+        /// </summary>
+        ProbabilityDensity,
+    }
+}
