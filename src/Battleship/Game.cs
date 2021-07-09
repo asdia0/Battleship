a2ba1258704e8c1c4e80007db1cb1fd04ab1f116
asdia0 <asdia.the.potato@gmail.com>
2021-07-10 00:32:30 +0800

    Move algorithms to separate class

--- src/Battleship/Game.cs
+++ src/Battleship/Game.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     /// <summary>
     /// Defines a game of Battleship.
@@ -73,22 +72,22 @@
             while (this.Winner == null)
             {
                 Square square = null;
-                (Player player, Grid p1, _) = this.ConfigureTurn();
+                (Player player, Grid p1, Grid p2) = this.ConfigureTurn();
 
                 if (this.Turn == Turn.Player1)
                 {
                     switch (player1Strategy)
                     {
                         case StrategyType.Random:
-                            square = this.Random();
+                            square = Strategy.Random(p2);
                             break;
 
                         case StrategyType.HuntTarget:
-                            square = this.HuntTarget();
+                            square = Strategy.HuntTarget(p1, p2);
                             break;
 
                         case StrategyType.ProbabilityDensity:
-                            square = this.ProbabilityDensity();
+                            square = Strategy.ProbabilityDensity(player, p1, p2);
                             break;
                     }
                 }
@@ -97,15 +96,15 @@
                     switch (player2Strategy)
                     {
                         case StrategyType.Random:
-                            square = this.Random();
+                            square = Strategy.Random(p2);
                             break;
 
                         case StrategyType.HuntTarget:
-                            square = this.HuntTarget();
+                            square = Strategy.HuntTarget(p1, p2);
                             break;
 
                         case StrategyType.ProbabilityDensity:
-                            square = this.ProbabilityDensity();
+                            square = Strategy.ProbabilityDensity(player, p1, p2);
                             break;
                     }
                 }
@@ -143,146 +142,6 @@
             {
                 return (this.Player2, this.Player2.Grid, this.Player1.Grid);
             }
-        }
-
-        /// <summary>
-        /// Attacks an enemy square randomly.
-        /// </summary>
-        /// <returns>The square to attack.</returns>
-        public Square Random()
-        {
-            Grid p2 = this.ConfigureTurn().grid2;
-
-            return p2.UnsearchedSquares[new Random().Next(p2.UnsearchedSquares.Count)];
-        }
-
-        /// <summary>
-        /// Attacks an enmy square that is adjacent to a hit square. Implements parity.
-        /// </summary>
-        /// <returns>The square to attack.</returns>
-        public Square HuntTarget()
-        {
-            Square attackedSquare;
-            (_, Grid p1, Grid p2) = this.ConfigureTurn();
-
-            if (p1.ToSearch.Count == 0)
-            {
-                // HUNT
-                attackedSquare = p2.UnsearchedSquares[new Random().Next(p2.UnsearchedSquares.Count)];
-            }
-            else
-            {
-                // TARGET
-                attackedSquare = p1.ToSearch.ToList()[new Random().Next(p1.ToSearch.Count)];
-            }
-
-            return attackedSquare;
-        }
-
-        /// <summary>
-        /// Attacks an enemy square based on previous searches. Searches for all enemy ships at the same time.
-        /// </summary>
-        /// <returns>The square to attack.</returns>
-        public Square ProbabilityDensity()
-        {
-            (Player player, Grid p1, Grid p2) = this.ConfigureTurn();
-
-            /*
-             * MUST
-             * 1. A hit square only has 1 unsearched adjacent square
-             * 2. A ship only has 1 possible arrangement
-             * MIGHT
-             * 3. A square has a hit adjacent square
-             * MUST NOT
-             * 4. Ships that are not sunk can't be located entirely on 'hit' squares.
-             */
-
-            if (p1.ToAttack.Any())
-            {
-                return p1.ToAttack.First();
-            }
-
-            // 1. A hit square only has 1 unsearched adjacent square
-            foreach (Square square in p2.Squares)
-            {
-                if (square.Status != SquareStatus.Hit)
-                {
-                    continue;
-                }
-
-                List<Square> unsearchedSquares = new ();
-
-                foreach (Square adjSquare in square.AdjacentSquares)
-                {
-                    if (!adjSquare.Searched)
-                    {
-                        unsearchedSquares.Add(adjSquare);
-                    }
-                }
-
-                if (unsearchedSquares.Count == 1)
-                {
-                    return unsearchedSquares[0];
-                }
-            }
-
-            // 2. A ship only has 1 possible arrangement
-            foreach (Ship ship in p2.OperationalShips)
-            {
-                HashSet<HashSet<int>> arrangements = ship.Arrangements;
-
-                if (arrangements.Count == 1)
-                {
-                    foreach (int id in arrangements.First().Where(i => p2.Squares[i].Status == SquareStatus.Unsearched))
-                    {
-                        p1.ToAttack.Add(p2.Squares[id]);
-                        return p1.ToAttack.First();
-                    }
-                }
-            }
-
-            // 3. A square has a hit adjacent square
-            Dictionary<Square, int> probability = new ();
-
-            if (p1.ToSearch.Count == 0)
-            {
-                // All unsearched squares
-                foreach (Square square in p2.Squares)
-                {
-                    {
-                        if (!square.Searched)
-                        {
-                            probability.Add(square, 0);
-                        }
-                    }
-                }
-            }
-            else
-            {
-                // All squares to search
-                foreach (Square square in p1.ToSearch)
-                {
-                    probability.Add(square, 0);
-                }
-            }
-
-            // Get probability
-            foreach (Ship ship in p2.OperationalShips)
-            {
-                foreach (HashSet<int> arrangement in ship.Arrangements)
-                {
-                    foreach (int squareID in arrangement)
-                    {
-                        Square sq = p2.Squares[squareID];
-                        if (probability.ContainsKey(sq))
-                        {
-                            probability[sq]++;
-                        }
-                    }
-                }
-            }
-
-            return probability.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
         }
 
         /// <summary>
