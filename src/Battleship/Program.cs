98ad5a92803a1db89632580671e4567a8991bec3
asdia0 <asdia.the.potato@gmail.com>
2021-07-09 16:19:40 +0800

    Add Program.RecordResults()

--- src/Battleship/Program.cs
+++ src/Battleship/Program.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
 
     /// <summary>
     /// Main class of the project.
@@ -13,14 +14,40 @@
         /// </summary>
         public static void Main()
         {
-            Simulate(1);
+            RecordResults(1000, "results.tsv");
+        }
+
+        /// <summary>
+        /// Simulates games and records win rates into a TSV files.
+        /// </summary>
+        /// <param name="numberOfGames">Number of games to simulate for each match.</param>
+        /// <param name="path">The path of the file to save to.</param>
+        public static void RecordResults(int numberOfGames, string path)
+        {
+            string res = "Player2\\Player1\tRandom\tHuntTarget\tProbabilityDensity";
+
+            for (int i = 0; i < 3; i++)
+            {
+                res += $"\n{(Strategy)i}";
+
+                for (int j = 0; j < 3; j++)
+                {
+                    (int, int) wins = Simulate(numberOfGames, (Strategy)j, (Strategy)i);
+                    res += $"\t{wins.Item1}";
+                }
+            }
+
+            File.WriteAllText(path, res);
         }
 
         /// <summary>
         /// Simulates a number of games.
         /// </summary>
         /// <param name="numberOfGames">Number of games to simulate.</param>
-        public static void Simulate(int numberOfGames)
+        /// <param name="player1strategy">Player 1' strategy.</param>
+        /// <param name="player2strategy">Player 2's strategy.</param>
+        /// <returns>A tuple of the number of wins each player had.</returns>
+        public static (int Player1, int Player2) Simulate(int numberOfGames, Strategy player1strategy, Strategy player2strategy)
         {
             Console.WriteLine($"Simulating {numberOfGames} games...");
             int first = 0;
@@ -46,7 +73,7 @@
 
                 Player p1 = new ("Player 1", g1);
                 Player p2 = new ("Player 2", g2);
-                Game g = new (p1, p2);
+                Game g = new (p1, p2, player1strategy, player2strategy);
 
                 if (g.Winner.Name == "Player 1")
                 {
@@ -58,7 +85,7 @@
                 }
             }
 
-            Console.WriteLine($"The first player won {first} games.\nThe second player won {second} games.");
+            return (first, second);
         }
     }
 }
